25699cc3fc892ff70dbba56ef00051c9c1276145
hrkt <hiroki110@gmail.com>
2020-07-18 07:03:24 +0900

    first commit

---
+++ src/test/java/info/beambitious/sandbox/booth2jpademo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package info.beambitious.sandbox.booth2jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
